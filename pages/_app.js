f7f0f81206cb5947fb0d8ddce263587ddab57800
zekal02 <rlaehdrnr002@naver.com>
2025-11-24 12:49:51 +0900

    광고 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css';
+// pages/_app.js
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        {/* AdSense 전역 스크립트 */}
+        <script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
